76ad2fd97eb427847ed56929cb33af20683da38b
Zhivanka <zivanka.miteva@gmail.com>
2022-11-21 10:55:06 +0100

    new version

---
+++ Cargo4You/Data/CargoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cargo4You.Models;
+
+namespace Cargo4You.Data
+{
+    public class CargoDbContext : DbContext
+    {
+        public CargoDbContext(DbContextOptions<CargoDbContext> options) : base(options)
+        { }
+
+        public DbSet<UserActivityModel> UserActivities { get; set; }
+    }
+}
