1a4b1517d2063b3517f722140f0eed7713a489e8
Zhivanka <71983871+Zhivanka@users.noreply.github.com>
2022-11-21 11:00:41 +0100

    Update CalculationController.cs

--- Cargo4You/Controllers/CalculationController.cs
+++ Cargo4You/Controllers/CalculationController.cs
@@ -1,63 +1,111 @@
-﻿using Cargo4You.Models;
+using Cargo4You.Models;
+using Cargo4You.Data;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System;
-using System.Net.Http.Headers;
+using System.IO;
 
 namespace Cargo4You.Controllers
 {
     public class CalculationController : Controller
     {
 
-        //Takes the input data from the client, call methods for calculate results, get api
+        public CalculationController()
+        {
+ 
+        }
+        //Takes the input data from the client, call all the methotds for validate and calulate
         //Returns a view with model dictionary results
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Result(InputDataModel obj)
         {
 
+            
             if (ModelState.IsValid)
             {
                 //Take all the data from the client input
-                double DimensionResult = CalculateVolume(obj.Depth, obj.Width, obj.Height);
-                double Weight = obj.Weight;
+                double Depth = Convert.ToDouble(obj.Depth);
+                double Width = Convert.ToDouble(obj.Width);
+                double Height = Convert.ToDouble(obj.Height);
+                double Weight = Convert.ToDouble(obj.Weight);
 
-                //Create model object for our company cargo
-                //Make the calculation and set property FinalPrice
-                Cargo4YouModel cargo4YouObj = new Cargo4YouModel();
-                cargo4YouObj.FinalPrice = Cargo4YouCalculation(cargo4YouObj, DimensionResult, Weight);
-
-                //Create list of partners, objects from partner model class
-                var partners = new List<PartnerModel>() {
-             new PartnerModel(){ Name="Ship Faster",
-                 BaseUrl="http://localhost:5164/",
-                 ApiControler="Api"},
-             new PartnerModel(){ Name="Malta Ship",
-                 BaseUrl="http://localhost:5163/",
-                 ApiControler="Api"}
-            };
+                //calculate volume of the parcel
+                double DimensionResult = CalculateVolume(Depth, Width, Height);
+                
+                //Create list of partners, read all the data from json file with GetCargoObject metod 
+                List<CargoModel>? partners = GetCargoObject();
 
                 //All the results goes in Dictionary (name, price) - we want to retrieve name and price in the view
-                Dictionary<string, double> results = new Dictionary<string, double>();
-                results.Add("Cargo 4 You", cargo4YouObj.FinalPrice);
+                Dictionary<string, double> results = new();
 
-
-                //foreach loop - go throu all the partners, call Api request, put the result in the dictionary 
-                foreach (var partner in partners)
+                //first check if the partner list is not empty
+                if (partners != null)
                 {
-                    double resultApi = CallingWebApi(partner.BaseUrl, partner.ApiControler, DimensionResult, Weight).Result;
-                    results.Add(partner.Name, resultApi);
+                    //go throu all the partners, make the validation&calculation put the result in the dictionary 
+                    foreach (var partner in partners)
+                    {
+
+                        //validation
+                        bool isValidWeight = false;
+                        bool isValidDimension = false;
+                        for (int i = 0; i < partner.Validations.Length; i ++)
+                        {
+                            string validationString = "";
+                            if (partner.Validations[i].Contains("kg"))
+                            {
+                              validationString = partner.Validations[i].Split("kg").First();
+                              isValidWeight = Validate(Weight, validationString);
+                            }
+                            else if (partner.Validations[i].Contains("cm"))
+                            {
+                                validationString = partner.Validations[i].Split("cm").First();
+                                isValidDimension = Validate(DimensionResult, validationString);
+                            }
+
+                        }
+ 
+                       
+                        //if is valid kg and dimension, call calculate and add results to the dictionary
+                        if (isValidWeight && isValidDimension)
+                        {
+                            //make the calculation to get weight price
+                            double WeightPrice = 0;
+                            foreach (var e in partner.CalculationsWeight)
+                            {
+                              WeightPrice = Calculate(e.Key, e.Value, Weight);
+                              if (WeightPrice != 0) { break; }
+                            }
+
+                            //make the calculation to get dimenstion price
+                            double DimensionPrice = 0;
+                            foreach (var e in partner.CalculationsDimension)
+                            {
+                                DimensionPrice = Calculate(e.Key, e.Value, DimensionResult);
+                                if (DimensionPrice != 0) { break; }
+                            }
+
+                            //get the bigger price comparing the DimensionPrice and WeightPrice
+                            double Price = GetFinalPrice(DimensionPrice, WeightPrice);
+                            results.Add(partner.Name, Price);
+                        }
+                    }
+
+                    //sort the result lower to biggest price
+                    var sortedList = results.ToList();
+                    sortedList.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+
+
+                    //return a view, bind the view with the dictionary - we want to retrieve name and price in the view
+                    return View(sortedList);
                 }
-
-
-                //return a view, bind the view with the dictionary - we want to retrieve name and price in the view
-                return View(results);
+                else
+                    Console.WriteLine("no results");
             }
 
-            
-
-           return RedirectToAction("Index","Home");
+            //if the form is not regulary fulfilled
+            return RedirectToAction("Index", "Home");
         }
+
 
 
 
@@ -65,6 +113,8 @@ namespace Cargo4You.Controllers
         //method for calculate volume for the parcel
         public double CalculateVolume(double depth, double width, double height)
         {
+           
+            
             return depth * width * height;
         }
 
@@ -82,79 +132,158 @@ namespace Cargo4You.Controllers
         }
 
 
-        //Cargo4You calculation for the price
-        public double Cargo4YouCalculation(Cargo4YouModel cargo4YouObj, double dimensionResult, double weight)
+        //validation
+        public bool Validate( double weightOrDimension, string stringToValidate)
         {
-            double DimensionResult = dimensionResult;
-            double Weight = weight;
-            
-            //validation
-            if (Weight <= 20 && DimensionResult <= 2000)
+            string valueToCompare;
+            double dblvalueToCompare;
+
+            if (stringToValidate.Contains("<="))
             {
-                //based on dimension
-                if (DimensionResult <= 1000)
-                {
-                    cargo4YouObj.DimensionPrice = 10;
-                }
-                else if (DimensionResult > 1000 && DimensionResult <= 2000)
-                {
-                    cargo4YouObj.DimensionPrice = 20;
-                }
-
-                //based on weight
-                if (Weight <= 2)
-                { cargo4YouObj.WeightPrice = 15; }
-                else if (Weight > 2 && Weight <= 15)
-                { cargo4YouObj.WeightPrice = 18; }
-                else if (Weight > 15 && Weight <= 20)
-                { cargo4YouObj.WeightPrice = 35; }
-
-                //final price
-                cargo4YouObj.FinalPrice = GetFinalPrice(cargo4YouObj.DimensionPrice, cargo4YouObj.WeightPrice);
+                valueToCompare = stringToValidate.Split("<=").Last();
+                dblvalueToCompare = double.Parse(valueToCompare);
                 
+                int resultValue = Math.Sign(weightOrDimension.CompareTo(dblvalueToCompare));
+                if (resultValue == -1){return true;}
+                else if (resultValue == 0){return true;}
+                else if (resultValue == 1){return false;}
             }
-            return cargo4YouObj.FinalPrice;
-          
-         
 
+            else if (stringToValidate.Contains(">="))
+            {
+                valueToCompare = stringToValidate.Split(">=").Last();
+                dblvalueToCompare = double.Parse(valueToCompare);
+
+                int resultValue = Math.Sign(weightOrDimension.CompareTo(dblvalueToCompare));
+                if (resultValue == -1){return false;}
+                else if (resultValue == 0){return true;}
+                else if (resultValue == 1){return true;}
+            }
+
+            else if (stringToValidate.Contains('<'))
+            {
+                valueToCompare = stringToValidate.Split('<').Last();
+                dblvalueToCompare = double.Parse(valueToCompare);
+
+                int resultValue = Math.Sign(weightOrDimension.CompareTo(dblvalueToCompare));
+                if (resultValue == -1){return true;}
+                else if (resultValue == 0){return false;}
+                else if (resultValue == 1){return false;}
+            }
+
+            else if (stringToValidate.Contains('>'))
+            {
+                valueToCompare = stringToValidate.Split('>').Last();
+                dblvalueToCompare = double.Parse(valueToCompare);
+
+                int resultValue = Math.Sign(weightOrDimension.CompareTo(dblvalueToCompare));
+                if (resultValue == -1){return false;}
+                else if (resultValue == 0){return false;}
+                else if (resultValue == 1){return true;}
+            }
+
+
+           return false;
         }
 
 
 
-       //Call the web api and return total price
-        public async Task<double> CallingWebApi(string urlApi, string controlerApi, double Dimension, double Weight)
+        //calculate the result
+        public double Calculate(string calculateString, string calculationPrice, double weightOrDimension)
         {
-            double resultApi=0;
+         
+            double price;
 
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(urlApi);
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-                HttpResponseMessage getData = await client.GetAsync(controlerApi+ "/"+Dimension+"/"+Weight);
-
-                if (getData.IsSuccessStatusCode)
+                //if contains && we should check 2 cases
+                if (calculateString.Contains("&&"))
                 {
-                    string result = getData.Content.ReadAsStringAsync().Result;
-                    resultApi = JsonConvert.DeserializeObject<double>(result);
+
+                    string[] b = calculateString.Split("&&");
+                    bool isValid1 = Validate(weightOrDimension, b[0]);
+                    bool isValid2 = Validate(weightOrDimension, b[1]);
+                    if (isValid1 && isValid2)
+                    {
+                        price = double.Parse(calculationPrice);
+                        return price;
+                    }
+
                 }
+
+                //if contains + make the calculation for extra charge "if bigger then.."
+                else if (calculationPrice.Contains('+'))
+                { 
+                   double extraCharge=double.Parse(calculationPrice.Split('+').Last());
+                   bool isValid = Validate(weightOrDimension, calculateString);
+                    if (isValid)
+                    {
+                        string stringMaxPrice = calculateString.Split().Last();
+                        string stringPrice = calculationPrice.Split('+').First();
+                        price = double.Parse(stringPrice);
+                        double maxKg = double.Parse(calculateString.Split('>').Last());
+                        double diferenceForExtraCharge = weightOrDimension - maxKg;
+                        double finalResult = 0;
+                        if (diferenceForExtraCharge < 1)
+                        { finalResult = price + 0; }
+                        else
+                        { finalResult = price + (diferenceForExtraCharge * extraCharge); }
+
+                        return finalResult;
+
+                    }
+
+                }
+
+                //if neither of the above is true just validate the case and return the price
                 else
                 {
-                    Console.WriteLine("Error calling web API");
-                 
+                    bool isValid = Validate(weightOrDimension, calculateString);
+                    if (isValid)
+                    {
+                        price = double.Parse(calculationPrice);
+                        return price;
+                    }
+
                 }
             
-            
-            }
-
-            return resultApi;
+            return 0;
         }
 
 
+        //reads cargo objects from json file, deserialize them into cargoModel objects
+        public List<CargoModel>? GetCargoObject()
+        {
+            List<CargoModel>? cargoListObj = new ();
+            using (StreamReader reader = new("Data/PartnersData.json"))
+            {
+                string json = reader.ReadToEnd();
+                cargoListObj = JsonConvert.DeserializeObject<List<CargoModel>>(json);
 
-     }
-}
+                if(cargoListObj!=null)
+                return cargoListObj;
+
+
+            }
+
+            return null;
+
+        }
+
+
+       
+
+
+        
+       
+
+        }
+    }
+
+
+    
+
+
+
+     
 
 
     
